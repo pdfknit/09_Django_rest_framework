e299d0b0f26962c150dd88351c0524d90dfda922
Sonya Y <yasenkovasofia@gmail.com>
2022-04-13 18:02:27 +0300

    lesson_02

--- django_rest/django_rest/settings.py
+++ django_rest/django_rest/settings.py
@@ -35,18 +35,27 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todoapp',
     'rest_framework',
+    'corsheaders',
+
+    'todoapp',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# TODO:
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'django_rest.urls'
---
+++ django_rest/frontend/src/components/Users_list.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const UserItem = ({user}) => {
+    return (
+        <tr>
+            <td>
+                {user.firstname}
+            </td>
+            <td>
+                {user.lastname}
+            </td>
+            <td>
+                {user.email}
+            </td>
+        </tr>
+    )
+}
+
+const UserList = ({users}) => {
+    return (
+        <table>
+            <th>
+                Firstname
+            </th>
+            <th>
+                Lastname
+            </th>
+            <th>
+                Email
+            </th>
+            { users.map((user) => <UserItem user={user} />)}
+        </table>
+    )
+}
+
+export default UserList
